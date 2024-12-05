1edbc0967a4ff5c9fffc93ece2b6aa519a37db4f
Lahiru Madhusanka <hhlahirumadusanka@gmail.com>
2024-12-06 00:44:44 +0200

    Add web form filling feature

--- tasks.py
+++ tasks.py
@@ -1,13 +1,16 @@
 from robocorp import browser
 from robocorp.tasks import task
+from datetime import datetime
 
 from RPA.HTTP import HTTP
+from RPA.PDF import PDF
 
 ARTEFACTS = "artefacts"
 REMOTE_EXPENSES_FILE = "https://fspacheco.github.io/rpa-challenge/assets/list-expenses.txt"
 LOCAL_EXPENSES_FILE = f"{ARTEFACTS}\latest-expenses.txt"
 TRACKER_APP = "https://fspacheco.github.io/rpa-challenge/expense-tracker.html"
 OUTPUT_REPORT = "expenses-report.pdf"
+DEBUG_MODE=True
 
 @task
 def robot_expense_tracker():
@@ -21,7 +24,7 @@ def robot_expense_tracker():
     browser.configure(
         browser_engine="chromium", 
         screenshot="only-on-failure", 
-        headless=True,
+        headless=False if DEBUG_MODE else True,
         slowmo=50
     )
     try:
@@ -31,13 +34,15 @@ def robot_expense_tracker():
         # Read and get sanitized data
         data = read_text_file(LOCAL_EXPENSES_FILE)
         
+        open_the_expense_tacking_website(TRACKER_APP)
+        fill_form_with_list(data)
+        
         print(data)
     except Exception as e:
         return f"Error: An unexpected error occurred. {e}"
     finally:
         # A place for teardown and cleanups. (Playwright handles browser closing)
         print("Automation finished!")
-
 
 def download_file(url, save_to):
     """
@@ -64,12 +69,12 @@ def read_text_file(file_path):
     
     data = []
     categories = {
-        "food": ["food", "pizza", "lunch", "dinner"],
-        "utilities": ["utilities", "util", "utilit"],
-        "entertainment": ["Entertainment", "entert"],
-        "shopping": ["shopping", "shop", "shoping"],
-        "transport": ["transport", "trasport", "tranport"],
-        "other": ["other", "othr", "oter"]
+        "Food": ["food", "pizza", "lunch", "dinner"],
+        "Utilities": ["utilities", "util", "utilit"],
+        "Entertainment": ["Entertainment", "entert"],
+        "Shopping": ["shopping", "shop", "shoping"],
+        "Transportation": ["transport", "trasport", "tranport"],
+        "Other": ["other", "othr", "oter"]
     }
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file: 
@@ -78,6 +83,9 @@ def read_text_file(file_path):
             
             if (len(columns) != 4):
                 raise Exception("Invalid data in the text file")
+            
+            # Format date
+            columns[0] = f"{columns[0]}/{datetime.now().year}"
             
             # Correct spelling mistakes in the category names
             is_changed = 0
@@ -92,3 +100,44 @@ def read_text_file(file_path):
             data.append(columns)
 
     return data
+
+def open_the_expense_tacking_website(url):
+    """
+    Navigates to the given URL
+    
+    Args: 
+        url: URL of the website need to be opened.
+    """
+    
+    browser.goto(url)
+    
+def fill_form_with_list(data):
+    """
+    Fill the expenses form
+    
+    Args:
+        data: List of sanitized data 
+    """
+    
+    for row in data:
+        page = browser.page()
+
+        page.fill("#description", row[1])
+        page.fill("#amount", row[2])
+        page.fill("#date", str(row[0]))
+        page.select_option("#category", str(row[3]))
+        page.click("text=Add Expense")
+        
+def export_as_pdf():
+    """
+    Export data to a PDF file
+    
+    Args:
+        data: List of sanitized data
+    """
+    
+    page = browser.page()
+    expenses_history_html = page.locator("#expenses-list").inner_html()
+    
+    pdf = PDF()
+    pdf.html_to_pdf(expenses_history_html, "output/expenses-history-report.pdf")
